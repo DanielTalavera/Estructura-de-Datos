7bf8bc3e801ade51c7db785c9aeac8a4411740e3
DanielTalavera <69166077+DanielTalavera@users.noreply.github.com>
2021-08-24 13:47:07 -0400

    Create z1 triangulo.h

---
+++ z1 triangulo.h
@@ -0,0 +1,14 @@
+#ifndef TRIANGULO_H
+#define TRIANGULO_H
+class Triangulo
+{
+	private:
+		int base;
+		int altura;
+		int area;
+	public:
+		double areatriangulo(int a, int b);
+		Triangulo(void);
+	protected:
+};
+#endif
